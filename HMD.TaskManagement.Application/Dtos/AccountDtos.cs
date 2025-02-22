f767e93d3afacb492459c03feb147a2e8d9c3bd3
HMDSS <halilmertdeveliii@gmail.com>
2025-02-22 18:19:32 +0300

    record for login

---
+++ HMD.TaskManagement.Application/Dtos/AccountDtos.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMD.TaskManagement.Application.Dtos
+{
+    public record LoginDto(string UserName, string Password);
+}
--- PersonalManagementSystem/Controllers/AccountController.cs
+++ PersonalManagementSystem/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HMD.TaskManagement.Application.Dtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HMD.TaskManagement.UI.Controllers
 {
@@ -8,8 +9,14 @@ namespace HMD.TaskManagement.UI.Controllers
         {
             return View();
         }
-
+        [HttpGet]
         public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginDto dto)//record
         {
             return View();
         }
