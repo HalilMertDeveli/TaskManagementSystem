ee98a1466b3b2dfa464bf66c2879e7e03004d569
HMDSS <halilmertdeveliii@gmail.com>
2025-02-28 12:51:00 +0300

    singout is okey

--- HMD.TaskManagement.Application/obj/Debug/net8.0/HMD.TaskManagement.Application.AssemblyInfo.cs
+++ HMD.TaskManagement.Application/obj/Debug/net8.0/HMD.TaskManagement.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HMD.TaskManagement.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25a6dcd782a5353461368e81980f49cee6372bd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7064b44c0d0b9fe98047f1103d5022183be163b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HMD.TaskManagement.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HMD.TaskManagement.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
